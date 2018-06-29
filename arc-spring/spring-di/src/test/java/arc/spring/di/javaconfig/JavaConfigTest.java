ca2d522050a69aab248015b1910b8857bb9b10e8
Swin <heatre@163.com>
2018-06-29 22:54:56 +0800

    feat: 添加java配置bean代码测试类

--- arc-spring/spring-di/src/test/java/arc/spring/di/javaconfig/JavaConfigTest.java
+++ arc-spring/spring-di/src/test/java/arc/spring/di/javaconfig/JavaConfigTest.java
@@ -1,7 +1,5 @@
 package arc.spring.di.javaconfig;
 
-import arc.spring.di.javaconfig.RabbitConfig;
-import arc.spring.di.javaconfig.Animal;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
